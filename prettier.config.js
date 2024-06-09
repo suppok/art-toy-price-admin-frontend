ff3202a41860dde48f1995e29dfa51cc3300b16b
suppok <crystalpok@gmail.com>
2024-06-09 21:10:06 +0700

    init

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  tabWidth: 2,
+  printWidth: 100
+};
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <AppHeader />
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
